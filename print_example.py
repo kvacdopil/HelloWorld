c688f4a4839758222bc11534b4266376b8f7b76b
Kvacdopil <kvacdopil@yandex.ru>
2020-02-28 01:36:42 +0300

    Добавил новую переменную

--- print_example.py
+++ print_example.py
@@ -2,5 +2,8 @@ adjective = input ("Введите прилагательное: ")
 noun = input ("Введите существительное: ")
 verb = input ("Введите глагол прошедшего времени: ")
 animal = input ("Введите название животного: ")
+planet = input ("Введите название планеты: ")
 print ("Ваша чепуха:")
 print ("Этот", adjective, noun, verb, "на ленивую рыжую", animal)
+print (planet)
+
